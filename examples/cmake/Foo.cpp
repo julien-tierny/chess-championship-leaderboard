3f018ff480af2875e48664d931e6c9bb431d706e
Julien J Tierny <julien.tierny@gmail.com>
2024-11-27 06:36:51 +0100

    relocated cpp examples

--- examples/cmake/Foo.cpp
+++
@@ -1,8 +0,0 @@
-#include <Foo.h>
-
-Foo::Foo() {
-  std::cout << "Creating an object from the class foo..." << std::endl;
-}
-
-Foo::~Foo() {
-}
--- examples/cmake/Foo.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-class Foo {
-public:
-  Foo();
-  ~Foo();
-};
